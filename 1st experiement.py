f10080c4138897240897a3b14592c0bda4d66988
U.Sravan Kumara <kumarasravan32@gmail.com>
2026-08-31 16:05:15 +0530

    these are the files that are done in the python labs.

---
+++ 1st experiement.py
@@ -0,0 +1,51 @@
+              #python programing 1st experiement::1::
+
+# Write a python program to read two numbers and display their sum
+
+"""num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+result=num1+num2
+print(result)"""
+
+              #python programing 2nd experiement::2::
+
+ # Write a python program to display the square of a number
+
+"""num=int(input("Enter num"))
+Result = num*num
+print(Result)"""
+
+              #python programing 3rd experiement::3::
+
+  # Write a python program to calculate area and perimeter of a rectangle
+
+"""length=-9
+breadth=5
+area=length*breadth
+print("area:", area)
+perimeter=2*(length+breadth)
+print("perimeter:", perimeter)"""
+
+             #python programing 4th experiement::4::
+
+     #Write a python program to convert temperature from Celsius to Fahrenheit
+
+"""celsius=float(input("Enter temperature in Celsius: "))
+fahrenheit=(celsius * 9/5)+32
+print("Temperature in Fahrenheit:", fahrenheit)"""
+
+            #python programing 5th experiement::5::
+
+     # WRITE A PYTHON PROGRAM TO SWAP TWO NUMBERS WITHOUT USING A THIRD VAIRABBLE:
+ 
+"""a=int(input("Enter a number a: "))
+b=int(input("Enter a number b: "))
+a=a+b
+b=a-b   
+a=a-b
+print("After swapping the values : a =", a)
+print("After swapping the values : b =", b)"""
+
+           #python programming 6th experiment::6::
+
+    # Write a python program to calculate simple interest
---
+++ python data types.py
@@ -0,0 +1,36 @@
+int_data=25
+print(int_data)                                             #integer data type
+
+
+float_data=25.5
+print(float_data)                                             #float data type
+
+
+str_data="Namaste"
+print(str_data)                                              #string data type
+
+
+list_data=[1,2,3,4,5]
+print(list_data)                                           #list data type
+  
+
+tuple_data=("PARROT", "SPARROW", "EAGLE", "OWL")
+print(tuple_data)                                        #tuple data type
+
+
+dict_data={'JANUARY':31, 'FEBRUARY':28, 'MARCH':31}
+print(dict_data)                                       #dictionary data type
+
+
+set_data={"apple", "banana", "mango", "orange"}
+print(set_data)                                         #set data type
+
+
+bool_data=True
+print(bool_data)                                         #boolean data type
+
+
+set_data1={1,2,3,4,5,5}
+print(set_data1)                                         #set data type with duplicate values
+
+
