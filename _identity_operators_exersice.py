9a56a623356d06595bb2426dc8212ce662f3894c
U.Sravan Kumara <kumarasravan32@gmail.com>
2026-09-08 13:28:58 +0530

    Add identity and is not operator exercises

--- _identity_operators_exersice.py
+++ _identity_operators_exersice.py
@@ -1,5 +1,20 @@
-# Identity Operators Exercise
-a=[1,2,3]
+#IS OPERATOR
+a=[1,2,3,4]
 b=a
 result= a is b
-print("result of",b,"is",a,"is",result)
+print("result of ",a, "is ",b, "is :",result)
+
+a=[1,2,3,4]
+b=[1,2,3,4]
+result= a is b
+print("result of ",a, "is",b, "is :",result)
+# IS NOT OPERATOR
+a=[1,2,3,4]
+b=a 
+result=a is not b
+print("result of ",a, "is not",b, "is :",result)
+
+a=[1,2,3,4]
+b=[1,2,3,4]
+result = a is not b
+print("result of ",a, "is not",b, "is :",result)
