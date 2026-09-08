aa6ba57ddcd1f2a0d56644e2cb162bca159b92c4
U.Sravan Kumara <kumarasravan32@gmail.com>
2026-09-08 13:23:21 +0530

    Implement arithmetic operators examples
    Added examples for various arithmetic operations including addition, subtraction, multiplication, division, modulus, floor division, and exponentiation.

--- #ATHEMTIC_OPERATORS.py
+++ #ATHEMTIC_OPERATORS.py
@@ -1,11 +1,22 @@
-#ATHEMTIC OPERATORS EXAMPES:
+#arithmetic operators
+num1=int(input("Enter the value of num1: "))
+num2=int(input("enter the value of num2:"))
+sum=num1+num2 #addition (+)
+print("sum of the two numbers:",sum)
 
-#ADDITION OPERATOR
-"""a=2
-b=5
-print("sum of a and b is :",a+b)"""
+difference=num1-num2 #subtraction (-)
+print("difference between two numbers:",difference)
 
-#subtraction operrator
-a=2
-b=5
-print("substraction of a and b is :",a+b)
+product=num1*num2 #multiplication (*)
+print("product between two numbers:",product)
+
+division=num1/num2 #division (/)
+print("division between two numbers:",division)
+
+modulus=num1%num2 #modulus(%)
+print("modulus between two numbers:",modulus)
+floordivision=num1//num2 #floordivision(//)
+print("floordivision between two numbers:",floordivision)
+
+exponentiation=num1**num2 #exponentiation (**)
+print("exponentiation between two numbers:",exponentiation)
