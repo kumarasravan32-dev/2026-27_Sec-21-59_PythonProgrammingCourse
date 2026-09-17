60cc727f85d981028c30ff154a8da9c3c9fb20b2
U.Sravan Kumara <kumarasravan32@gmail.com>
2026-09-17 22:57:11 +0530

    Add files via upload

---
+++ Unit-1_Task-1_Sum_of_Two_Numbers.py.py
@@ -0,0 +1,8 @@
+# python programming 1st experiment::1::
+
+# Write a python program to read two numbers and display their sum
+
+num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+result=num1+num2
+print(result)
